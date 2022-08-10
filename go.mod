24a8078dd448e84b63cb2dfc75c6c1652de0a9d8
Richard Dominick <richdom2185@gmail.com>
2022-08-10 18:34:40 +0800

    Add ability to read UUID from STDIN

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/RichDom2185/uuidtob62
 
 go 1.18
+
+require github.com/andrew-d/go-termutil v0.0.0-20150726205930-009166a695a2
--- uuidtob62.go
+++ uuidtob62.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"math/big"
 	"os"
 	"strings"
+
+	termutil "github.com/andrew-d/go-termutil"
 )
 
 func parse(uuid string) {
@@ -16,16 +19,41 @@ func parse(uuid string) {
 	fmt.Println(b16.Text(62))
 }
 
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Missing argument: Please provide a file name")
-		return
-	}
+func readFromFile() []byte {
 	data, err := os.ReadFile(os.Args[1])
 	if err != nil {
 		fmt.Println("Error reading file:", err)
 		panic(err)
 	}
+	return data
+}
+
+func readFromSTDIN() []byte {
+	data, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		panic(err)
+	}
+	return data
+}
+
+func main() {
+	isSTDINAvailable := !termutil.Isatty(os.Stdin.Fd())
+	isArgumentAvailable := len(os.Args) == 2
+
+	if !isSTDINAvailable && !isArgumentAvailable {
+		fmt.Println("Incorrect usage: Either pass in a UUID string, or provide a file name.")
+		return
+	}
+
+	var data []byte
+	if isSTDINAvailable {
+		data = readFromSTDIN()
+	}
+	if isArgumentAvailable {
+		data = readFromFile()
+	}
+
 	original := strings.TrimSpace(string(data))
 	original = strings.ReplaceAll(original, "-", "")
 	parse(original)
